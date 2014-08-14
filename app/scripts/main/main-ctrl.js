ec50b2dc384bae44f4e56dc96515ae83eb286861
daniel <demise4u@gmail.com>
2014-08-14 17:27:43 -0500

    added map.js

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -49,7 +49,7 @@ angular.module('texquiz')
     {
       'key':'stratos',
       'description':'Stratos Capsule',
-      'source':'stratos.jpg'
+      'source':'stratos.jpg',
       'latitude': 29.554700,
       'longitude': -95.094287
     },
@@ -81,52 +81,52 @@ angular.module('texquiz')
   // };
   //call with ng-click on the index.html
 
-  $scope.returnScore = function(index) {
-    data = {
-      'latitude': ,
-      'longitude':
-    }; 
-    calculateDistance(userAnswer, data)
-  }
+  // $scope.returnScore = function(index) {
+  //   data = {
+  //     'latitude': ,
+  //     'longitude':
+  //   }; 
+  //   calculateDistance(userAnswer, data)
+  // }
 
-  function calculateDistance(userObject, dataObject) {
-    var lat1 = userObject.latitude,
-        lon1 = userObject.longitude,
-        lat2 = dataObject.latitude,
-        lon2 = dataObject.longitude;
-    getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2);
-  }
+  // function calculateDistance(userObject, dataObject) {
+  //   var lat1 = userObject.latitude,
+  //       lon1 = userObject.longitude,
+  //       lat2 = dataObject.latitude,
+  //       lon2 = dataObject.longitude;
+  //   getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2);
+  // }
 
   
 
-  function calculateScore(distanceInKm) {
-    if (distanceInKm <= 32) {
-      return 10;
-    } else if (distanceInKm >= 322) {
-      return 0;
-    } else {
-      var scaled = (distanceInKm - 32)/290;
-      return 10 - (scaled * 10);
-    }
-  }
+  // function calculateScore(distanceInKm) {
+  //   if (distanceInKm <= 32) {
+  //     return 10;
+  //   } else if (distanceInKm >= 322) {
+  //     return 0;
+  //   } else {
+  //     var scaled = (distanceInKm - 32)/290;
+  //     return 10 - (scaled * 10);
+  //   }
+  // }
 
-  function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2-lat1);  // deg2rad below
-    var dLon = deg2rad(lon2-lon1); 
-    var a = 
-      Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-      Math.sin(dLon/2) * Math.sin(dLon/2)
-      ; 
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    var d = R * c; // Distance in km
-    return d;
-  }
+  // function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  //   var R = 6371; // Radius of the earth in km
+  //   var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  //   var dLon = deg2rad(lon2-lon1); 
+  //   var a = 
+  //     Math.sin(dLat/2) * Math.sin(dLat/2) +
+  //     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+  //     Math.sin(dLon/2) * Math.sin(dLon/2)
+  //     ; 
+  //   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  //   var d = R * c; // Distance in km
+  //   return d;
+  // }
 
-  function deg2rad(deg) {
-    return deg * (Math.PI/180);
-  }  
+  // function deg2rad(deg) {
+  //   return deg * (Math.PI/180);
+  // }  
 
 
     // angular.forEach($scope.landmarks, function(awesomeThing) {
